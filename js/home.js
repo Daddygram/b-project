4f5a0dc70cbd9f630a5e791df0d12db7d7a89224
Joseph Moninyo <tsotne.s.2004@gmail.com>
2024-01-17 20:48:09 +0400

    pages-fix

--- js/home.js
+++ js/home.js
@@ -1,45 +1,40 @@
 // card sliders
 function setupSlider(containerSelector, prevBtnSelector, nextBtnSelector) {
-  const container = document.querySelector(containerSelector);
-  const slides = Array.from(container.children);
-  const prevBtn = document.querySelector(prevBtnSelector);
-  const nextBtn = document.querySelector(nextBtnSelector);
+  const container = document.querySelector(containerSelector)
+  const slides = Array.from(container.children)
+  const prevBtn = document.querySelector(prevBtnSelector)
+  const nextBtn = document.querySelector(nextBtnSelector)
 
-  let currentIndex = 0;
+  let currentIndex = 0
 
   slides.forEach((slide, index) => {
-    slide.style.order = index;
-  });
+    slide.style.order = index
+  })
 
   function nextSlide() {
     currentIndex = (currentIndex + 1) % slides.length;
-    updateSlides();
+    updateSlides()
   }
 
   function prevSlide() {
     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    updateSlides();
+    updateSlides()
   }
 
   function updateSlides() {
     slides.forEach((slide) => {
-      slide.style.order = currentIndex;
-    });
+      slide.style.order = currentIndex
+    })
     container.scrollTo({
       left: currentIndex * slides[0].offsetWidth,
       behavior: "smooth",
-    });
+    })
   }
 
-  prevBtn.addEventListener("click", prevSlide);
-  nextBtn.addEventListener("click", nextSlide);
+  prevBtn.addEventListener("click", prevSlide)
+  nextBtn.addEventListener("click", nextSlide)
 
-  // Initial update of the slides
-  updateSlides();
+  // update of the slides
+  updateSlides()
 }
 
-// Set up sliders
-setupSlider(".AllSlides", "#prevBtn1", "#nextBtn1");
-setupSlider("#AllSlides2", "#prevBtn2", "#nextBtn2");
-setupSlider("#AllSlides3", "#prevBtn3", "#nextBtn3");
-setupSlider("#AllSlides4", "#prevBtn4", "#nextBtn4");
